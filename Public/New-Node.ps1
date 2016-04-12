840903530611b67a332f1528191a7e1d8d8be4b3
Jason Vercellone <vercellone@earthlink.net>
2016-04-12 18:42:38 -0500

    Add *-Node
    Added functions to optionally manage Nodes directly rather than through
    *-PoolMember:
    ```PowerShell
    Get-Node
    New-Node
    Remove-Node
    ```
    This change introduces Private\New-F5Item to refactor out code common to
    all New-* functions except New-F5Session.  I'll follow up with another
    commit which leverages the New-F5Item in New-HealthMonitor, New-Pool,
    and New-VirtualServer.
    
    Remove-Node differs from existing Remove-* functions: It does not return
    a boolean.  Instead if an error occurs it will Write-Error.
    -ErrorAction is fully supported.  I believe this better aligns with the
    behavior of other Remove-* cmdlets.  More to come...

---
+++ Public/New-Node.ps1
@@ -0,0 +1,54 @@
+﻿Function New-Node {
+<#
+.SYNOPSIS
+    Create Node(s)
+
+.EXAMPLE
+    New-Node -Address 192.168.1.42
+#>
+    [cmdletBinding()]
+    param (
+        $F5Session=$Script:F5Session,
+
+        [Parameter(Mandatory=$true,ParameterSetName='Address',ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [string[]]$Address,
+
+        [Alias('ComputerName')]
+        [Alias('NodeName')]
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+        [string[]]$Name='',
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+        [string]$Partition,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+        [string[]]$Description='',
+
+        [switch]$Passthru
+    )
+    begin {
+        #Test that the F5 session is in a valid format
+        Test-F5Session($F5Session)
+
+        $URI = ($F5Session.BaseURL + "node")
+    }
+    process {
+        for ([int]$a=0; $a -lt $Address.Count; $a++) {
+            $itemname = ($Name[$a],$Address[$a] -ne '')[0]
+            $newitem = New-F5Item -Name $itemname -Partition $Partition 
+            #Check whether the specified node already exists
+            If (Test-Node -F5session $F5Session -Name $newitem.Name -Partition $newitem.Partition){
+                Write-Error "The $($newitem.FullPath) node already exists."
+            } else {
+                #Start building the JSON for the action
+                $JSONBody = @{address=$Address[$a];name=$newitem.Name;partition=$newitem.Partition;description=$Description[$a]} | ConvertTo-Json
+
+                Invoke-RestMethodOverride -Method POST -Uri "$URI" -Credential $F5Session.Credential -Body $JSONBody -ContentType 'application/json' |
+                    Out-Null
+                if ($Passthru) {
+                    Get-Node -F5Session $F5Session -Name $newitem.Name -Partition $newitem.Partition
+                }
+            }
+        }
+    }
+}
---
+++ Public/Remove-Node.ps1
@@ -0,0 +1,51 @@
+﻿Function Remove-Node {
+<#
+.SYNOPSIS
+    Remove the specified node(s)
+.NOTES
+    Node names are case-specific.
+#>
+    [cmdletBinding( SupportsShouldProcess=$true, ConfirmImpact='Medium')]
+    param (
+        $F5Session=$Script:F5Session,
+
+        [Alias('Node')]
+        [Parameter(Mandatory=$true,ParameterSetName='InputObject',ValueFromPipeline=$true)]
+        [PSObject[]]$InputObject,
+
+        [Parameter(Mandatory=$true,ParameterSetName='Address',ValueFromPipelineByPropertyName=$true)]
+        [string[]]$Address='*',
+
+        [Alias('ComputerName')]
+        [Alias('NodeName')]
+        [Parameter(Mandatory=$true,ParameterSetName='Name',ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [string[]]$Name,
+
+        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
+        [string]$Partition
+    )
+    begin {
+        #Test that the F5 session is in a valid format
+        Test-F5Session($F5Session)
+
+        Write-Verbose "NB: Node names are case-specific."
+    }
+    process {
+        switch($PSCmdLet.ParameterSetName) {
+            Address {
+                Get-Node -F5Session $F5Session -Address $Address -Partition $Partition | Remove-Node -F5session $F5Session
+            }
+            Name {
+                $Name | Get-Node -F5Session $F5Session -Address $Address -Partition $Partition | Remove-Node -F5session $F5Session
+            }
+            InputObject {
+                foreach($item in $InputObject) {
+                    if ($pscmdlet.ShouldProcess($item.fullPath)){
+                        $URI = $F5Session.GetLink($item.selfLink)
+                        Invoke-RestMethodOverride -Method DELETE -Uri $URI -Credential $F5Session.Credential
+                    }
+                }
+            }
+        }
+    }
+}
