969e048a826b62728d3c64717577290283ad9cf8
Jason Vercellone <vercellone@earthlink.net>
2016-04-12 16:56:10 -0500

    Get-PoolMemberStats
    Added Get-PoolMemberStats
    Deprecated Get-CurrentConnectionCount

---
+++ Public/Deprecated/Get-CurrentConnectionCount.ps1
@@ -0,0 +1,23 @@
+﻿Function Get-CurrentConnectionCount {
+<#
+.SYNOPSIS
+    Get-CurrentConnectionCount is deprecated.  Please use Get-PoolMemberStats | Select-Object -ExpandProperty 'serverside.curConns'
+#>
+    param(
+        $F5Session=$Script:F5Session,
+
+        [Parameter(Mandatory=$true)]
+        [string[]]$PoolName,
+        [Parameter(Mandatory=$false)]
+        [string]$Partition,
+
+        [Alias("ComputerName")]
+        [Parameter(Mandatory=$false)]
+        [string]$Address='*',
+
+        [Parameter(Mandatory=$false)]
+        [string]$Name='*'
+    )
+    Write-Warning "Get-CurrentConnectionCount is deprecated.  Please use Get-PoolMemberStats | Select-Object -ExpandProperty 'serverside.curConns'"
+    Get-PoolMemberStats -F5Session $F5Session -PoolName $PoolName -Partition $Partition -Address $Address -Name $Name | Select-Object -ExpandProperty 'serverside.curConns'
+}
