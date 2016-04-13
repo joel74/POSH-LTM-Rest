cfa5dedb47c93407dac75e77e69d4019d6205394
Jason Vercellone <vercellone@earthlink.net>
2016-04-12 19:20:45 -0500

    Get-HealthMonitorType
    Added optional Name filter
    Pipeline enabled

--- Public/Get-HealthMonitorType.ps1
+++ Public/Get-HealthMonitorType.ps1
@@ -1,18 +1,28 @@
 ﻿Function Get-HealthMonitorType {
 <#
 .SYNOPSIS
-    Get a list of all health monitor types for the specified F5 LTM
+    Retrieve the specified health monitor type(s).
 #>
     [cmdletBinding()]
     param (
-        $F5Session=$Script:F5Session        
+        $F5Session=$Script:F5Session,
+
+        [Alias('Type')]
+        [Parameter(Mandatory=$false,ValueFromPipeline=$true,ValueFromPipelineByPropertyName=$true)]
+        [string[]]$Name='*'
     )
-    #Test that the F5 session is in a valid format
-    Test-F5Session($F5Session)
-
-    #Only retrieve the Health monitor types
-    $URI = $F5Session.BaseURL + "monitor/"
-
-    $monitors = Invoke-RestMethodOverride -Method Get -Uri $URI -Credential $F5Session.Credential -ErrorMessage "Failed to get the list of healt monitor types."
-    $monitors.items.reference | ForEach-Object { [Regex]::Match($_.Link,'(?<=/)[^/?]*(?=\?)').Value }
+    begin {
+        #Test that the F5 session is in a valid format
+        Test-F5Session($F5Session)
+    }
+    process {
+        foreach ($itemname in $Name) {
+            $URI = $F5Session.BaseURL + 'monitor/'
+            $JSON = Invoke-RestMethodOverride -Method Get -Uri $URI -Credential $F5Session.Credential
+            $JSON.items.reference | ForEach-Object { 
+                [Regex]::Match($_.Link,'(?<=/)[^/?]*(?=\?)').Value |
+                    Where-Object { $_ -like $itemname }
+            }
+        }
+    }    
 }
