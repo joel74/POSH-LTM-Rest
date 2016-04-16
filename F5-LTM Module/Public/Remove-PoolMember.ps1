4fbec7f3534cf8529e2fd6cd359394b3b6409a2c
Jason Vercellone <vercellone@earthlink.net>
2016-04-15 20:54:48 -0500

    Moved module contents into F5-LTM Module folder to support Publish-Module.

---
+++ F5-LTM Module/Public/Remove-PoolMember.ps1
@@ -0,0 +1,61 @@
+﻿Function Remove-PoolMember {
+<#
+.SYNOPSIS
+    Remove member node(s) from a pool
+.NOTES
+    Pool and member names are case-specific.
+#>
+    [cmdletBinding( SupportsShouldProcess=$true, ConfirmImpact='High')]
+    param (
+        $F5Session=$Script:F5Session,
+
+        # InputObject could be Pool objects, but should ultimately be PoolMember objects
+        [Alias('Pool')]
+        [Alias('PoolMember')]
+        [Parameter(Mandatory=$true,ParameterSetName='InputObject',ValueFromPipeline=$true)]
+        [PSObject[]]$InputObject,
+
+        [Parameter(Mandatory=$true,ParameterSetName='PoolName',ValueFromPipeline=$true)]
+        [string[]]$PoolName,
+
+        [Parameter(Mandatory=$false,ParameterSetName='PoolName')]
+        [string]$Partition,
+
+        [Alias("ComputerName")]
+        [Parameter(Mandatory=$false)]
+        [string[]]$Address='*',
+
+        [Parameter(Mandatory=$false)]
+        [string[]]$Name='*'
+    )
+    begin {
+        #Test that the F5 session is in a valid format
+        Test-F5Session($F5Session)
+    }
+    process {
+        switch($PSCmdLet.ParameterSetName) {
+            InputObject {
+                foreach($item in $InputObject) {
+                    switch ($item.kind) {
+                        "tm:ltm:pool:poolstate" {
+                            if ($Address -or $Name) {
+                                $InputObject | Get-PoolMember -F5session $F5Session -Address $Address -Name $Name | Remove-PoolMember -F5session $F5Session
+                            } else {
+                                Write-Error 'Address and/or Name is required when the pipeline object is not a PoolMember'
+                            }
+                        }
+                        "tm:ltm:pool:members:membersstate" {
+                            if ($pscmdlet.ShouldProcess($item.GetFullName())) {
+                                $URI = $F5Session.GetLink($item.selfLink)
+                                Invoke-RestMethodOverride -Method DELETE -Uri $URI -Credential $F5Session.Credential
+                            }
+                        }
+                    }
+                }
+            }
+            PoolName {
+                Get-PoolMember -F5session $F5Session -PoolName $PoolName -Partition $Partition -Address $Address -Name $Name | Remove-PoolMember -F5session $F5Session
+            }
+        }
+    }
+}
---
+++ F5-LTM Module/Public/Set-PoolMemberDescription.ps1
@@ -0,0 +1,53 @@
+﻿Function Set-PoolMemberDescription {
+<#
+.SYNOPSIS
+    Set the description value for the specified pool member
+#>
+    [cmdletBinding()]
+    param(
+        $F5Session=$Script:F5Session,
+
+        [Parameter(Mandatory=$true,ParameterSetName='InputObject',ValueFromPipeline=$true)]
+        [Alias('PoolMember')]
+        [PSObject[]]$InputObject,
+
+        [Parameter(Mandatory=$true,ParameterSetName='PoolName',ValueFromPipeline=$true)]
+        [string[]]$PoolName,
+        [Parameter(Mandatory=$false,ParameterSetName='PoolName')]
+        [string]$Partition,
+
+        [Alias("ComputerName")]
+        [Parameter(Mandatory=$false,ParameterSetName='InputObject')]
+        [Parameter(Mandatory=$true,ParameterSetName='PoolName')]
+        [string]$Address='*',
+
+        [string]$Name='*',
+        
+        [Parameter(Mandatory=$true)]$Description
+    )
+    process {
+        switch($PSCmdLet.ParameterSetName) {
+            InputObject {
+                switch ($InputObject.kind) {
+                    "tm:ltm:pool:poolstate" {
+                        if (!$Address) {
+                            Write-Error 'Address is required when the pipeline object is not a PoolMember'
+                        } else {
+                            $InputObject | Get-PoolMember -F5session $F5Session -Address $Address -Name $Name | Set-PoolMemberDescription -F5session $F5Session
+                        }
+                    }
+                    "tm:ltm:pool:members:membersstate" {
+                        foreach($member in $InputObject) {
+                            $JSONBody = @{description=$Description} | ConvertTo-Json
+                            $URI = $F5Session.GetLink($member.selfLink)
+                            Invoke-RestMethodOverride -Method PATCH -Uri "$URI" -Credential $F5Session.Credential -Body $JSONBody -ContentType 'application/json' -ErrorMessage "Failed to set the description on $ComputerName in the $PoolName pool to $Description." -AsBoolean
+                        }
+                    }
+                }
+            }
+            PoolName {
+                Get-PoolMember -F5session $F5Session -PoolName $PoolName -Partition $Partition -Address $Address -Name $Name | Set-PoolMemberDescription -F5session $F5Session -Description $Description
+            }
+        }
+    }
+}
