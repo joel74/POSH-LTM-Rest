2356c39d3825deaca000466f6b7eb66aab5a7291
joel <jnewton@springcm.com>
2018-05-01 23:12:02 -0500

    Made some changes / additions to the example and updated the params with validation sets and some different variable types.

--- F5-LTM/Public/New-ProfileHttp.ps1
+++ F5-LTM/Public/New-ProfileHttp.ps1
@@ -4,13 +4,13 @@ Function New-ProfileHttp {
     Create a new profile.
 
 .DESCRIPTION
-    readmore about f5 Api
+    Read more about http profiles 
     https://devcentral.f5.com/Wiki/iControlREST.APIRef_tm_ltm_profile_http.ashx
-
+	
 .EXAMPLE
-    New-Profile -F5Session $F5Session -Name $ProfilName -Partition $Partition -insertXforwardedFor "enabled"
+    New-Profile -F5Session $F5Session -Name $ProfileName -Partition $Partition -insertXforwardedFor "Enabled"
 .EXAMPLE
-    New-Profile -F5Session $F5Session -Name "Service1" -Partition "ADO" -insertXforwardedFor "enabled" 
+    New-Profile -F5Session $F5Session -Name "Service1" -Partition "ADO" -insertXforwardedFor "Enabled" 
 .EXAMPLE
     Result of RestAPI 
     kind                      : tm:ltm:profile:http:httpstate
@@ -19,36 +19,36 @@ Function New-ProfileHttp {
     fullPath                  : /ADO/http_ServiceGB123
     generation                : 7645
     selfLink                  : https://localhost/mgmt/tm/ltm/profile/http/~ADO
-    acceptXff                 : disabled
-    appService                : none
-    basicAuthRealm            : none
+    acceptXff                 : Disabled
+    appService                : None
+    basicAuthRealm            : None
     defaultsFrom              : /Common/http
     defaultsFromReference     : @{link=https://localhost/mgmt/tm/ltm/profile/ht
-    description               : none
+    description               : None
     encryptCookieSecret       : ****
     encryptCookies            : {}
     enforcement               : @{excessClientHeaders=reject; excessServerHeade
-                                truncatedRedirects=disabled; unknownMethod=allo
-    explicitProxy             : @{badRequestMessage=none; badResponseMessage=no
-    fallbackHost              : none
+                                truncatedRedirects=Disabled; unknownMethod=Allo
+    explicitProxy             : @{badRequestMessage=None; badResponseMessage=no
+    fallbackHost              : None
     fallbackStatusCodes       : {}
-    headerErase               : none
-    headerInsert              : none
-    hsts                      : @{includeSubdomains=enabled; maximumAge=1607040
-    insertXforwardedFor       : enabled
-    lwsSeparator              : none
+    headerErase               : None
+    headerInsert              : None
+    hsts                      : @{includeSubdomains=Enabled; maximumAge=1607040
+    insertXforwardedFor       : Enabled
+    lwsSeparator              : None
     lwsWidth                  : 80
-    oneconnectTransformations : enabled
+    oneconnectTransformations : Enabled
     proxyType                 : reverse
-    redirectRewrite           : none
-    requestChunking           : preserve
-    responseChunking          : selective
+    redirectRewrite           : None
+    requestChunking           : Preserve
+    responseChunking          : Selective
     responseHeadersPermitted  : {}
     serverAgentName           : BigIP
     sflow                     : @{pollInterval=0; pollIntervalGlobal=yes; sampl
-    viaHostName               : none
-    viaRequest                : preserve
-    viaResponse               : preserve
+    viaHostName               : None
+    viaRequest                : Preserve
+    viaResponse               : Preserve
     xffAlternativeNames       : {}
 #>   
     [cmdletBinding()]
@@ -60,30 +60,36 @@ Function New-ProfileHttp {
         
         [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true)]
         [string]$Partition,
+        [ValidateSet('enabled','disabled')]
         [string]$acceptXff,
         [string]$appService,
         [string]$basicAuthRealm,
         [string]$description,
         [string]$encryptCookieSecret,
-        [string]$encryptCookies,
+        [string[]]$encryptCookies=@(),
         [string]$fallbackHost,
-        [string]$fallbackStatusCodes,
+        [string[]]$fallbackStatusCodes=@(),
         [string]$headerErase,
         [string]$headerInsert,
-        [ValidateSet('enabled','disabled')]
+        [ValidateSet('Enabled','Disabled')]
         [string]$insertXforwardedFor,
         [string]$lwsSeparator,
         [int]$lwsWidth,
         [string]$oneconnectTransformations,
         [string]$tmPartition,
         [string]$proxyType,
+		[ValidateSet('None','All','Matching','Nodes')]
         [string]$redirectRewrite,
+		[ValidateSet('Preserve','Selective','Rechunk')]
         [string]$requestChunking,
+		[ValidateSet('Preserve','Selective','Unchunk','Rechunk')]
         [string]$responseChunking,
         [string]$responseHeadersPermitted,
         [string]$serverAgentName,
         [string]$viaHostName,
+		[ValidateSet('Remove','Preserve','Append')]
         [string]$viaRequest,
+		[ValidateSet('Remove','Preserve','Append')]
         [string]$viaResponse,
         [string]$xffAlternativeNames,
         [string]$Enforcement
@@ -106,10 +112,12 @@ Function New-ProfileHttp {
                 #Start building the JSON for the action
                 if($Enforcement -eq $null ){$Enforcement = @{}}
                 $JSONBody = @{name=$newitem.Name;partition=$newitem.Partition;acceptXff=$acceptXff;appService=$appService;basicAuthRealm=$basicAuthRealm;defaultsFrom="/Common/http";description=$description;encryptCookieSecret=$encryptCookieSecret;encryptCookies=$encryptCookies;fallbackHost=$fallbackHost;fallbackStatusCodes=$fallbackStatusCodes;headerErase=$headerErase;headerInsert=$headerInsert;insertXforwardedFor=$insertXforwardedFor;lwsSeparator=$lwsSeparator;lwsWidth=$lwsWidth;oneconnectTransformations=$oneconnectTransformations;tmPartition=$tmPartition;proxyType=$proxyType;redirectRewrite=$redirectRewrite;requestChunking=$requestChunking;responseChunking=$responseChunking;responseHeadersPermitted=$responseHeadersPermitted;serverAgentName=$serverAgentName;viaHostName=$viaHostName;viaRequest=$viaRequest;viaResponse=$viaResponse;xffAlternativeNames=$xffAlternativeNames;Enforcement=$Enforcement}
-                ($JSONBody.GetEnumerator() | ? Value -eq "" ).name | % {$JSONBody.remove($_)}
+                ($JSONBody.GetEnumerator() | ? Value -eq "" ).name | % {$JSONBody.Remove($_)}
                 $JSONBody = $JSONBody | ConvertTo-Json
                 Invoke-F5RestMethod -Method POST -Uri "$URI" -F5Session $F5Session -Body $JSONBody -ContentType 'application/json' -ErrorMessage ("Failed to create the $($newitem.FullPath) profile.") -AsBoolean
-            }
+				Write-Verbose "If viaRequest or viaResponse is set to 'append,' then a value for viaHostName is required."
+
+			}
         }
     }
 }
