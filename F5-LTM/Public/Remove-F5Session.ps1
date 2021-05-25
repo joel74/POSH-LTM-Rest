655671ce3638fcf0512810e43a85b33f8cbb5106
Adeel ASIF <adeel.asif-ext@fnacdarty.com>
2021-05-25 22:55:24 +0200

    Add error handling  + return $true

--- F5-LTM/Public/Remove-F5Session.ps1
+++ F5-LTM/Public/Remove-F5Session.ps1
@@ -1,33 +1,44 @@
 Function Remove-F5Session {
-    <#
-    .SYNOPSIS
-        Remove F5 session based on the token
-    #>
+<#
+.SYNOPSIS
+    Remove F5 session based on the token
+#>
     [cmdletBinding()]
     param (
         $F5Session = $Script:F5Session,
-        [switch]$SkipCertifcateCheck
+        [switch]$SkipCertificateCheck
     )
     #Validate F5Session 
     Test-F5Session($F5Session)
+
+    try {
     
-    if ($SkipCertifcateCheck) {
+        if ($SkipCertificateCheck) {
 
-        if ($PSVersionTable.PSVersion.Major -ge 6) {
+            if ($PSVersionTable.PSVersion.Major -ge 6) {
 
-            $RemoveSession = Invoke-RestMethod "https://$($F5Session.name)/mgmt/shared/authz/tokens/$($F5Session.token)" -Headers @{'X-F5-Auth-Token' = $F5Session.token } -Method DELETE -SkipCertificateCheck
+                $RemoveSession = Invoke-RestMethod "https://$($F5Session.name)/mgmt/shared/authz/tokens/$($F5Session.token)" -Headers @{'X-F5-Auth-Token' = $F5Session.token } -Method DELETE -SkipCertificateCheck -ErrorVariable LTMError
+            }
+            else {
+
+                [SSLValidator]::OverrideValidation()
+                $RemoveSession = Invoke-RestMethod "https://$($F5Session.name)/mgmt/shared/authz/tokens/$($F5Session.token)" -Headers @{'X-F5-Auth-Token' = $F5Session.token } -Method DELETE -ErrorVariable LTMError
+                [SSLValidator]::RestoreValidation()
+            }
         }
         else {
 
-            [SSLValidator]::OverrideValidation()
-            $RemoveSession = Invoke-RestMethod "https://$($F5Session.name)/mgmt/shared/authz/tokens/$($F5Session.token)" -Headers @{'X-F5-Auth-Token' = $F5Session.token } -Method DELETE
-            [SSLValidator]::RestoreValidation()
+            $RemoveSession = Invoke-RestMethod "https://$($F5Session.name)/mgmt/shared/authz/tokens/$($F5Session.token)" -Headers @{'X-F5-Auth-Token' = $F5Session.token } -Method DELETE -ErrorVariable LTMError
         }
-    }
-    else {
 
-        $RemoveSession = Invoke-RestMethod "https://$($F5Session.name)/mgmt/shared/authz/tokens/$($F5Session.token)" -Headers @{'X-F5-Auth-Token' = $F5Session.token } -Method DELETE
+        Write-Verbose "Session : token $($RemoveSession.token) deleted"
+        Return($true)
     }
 
-    Write-Verbose "Session : token $($RemoveSession.token) deleted"
+    catch {
+
+        $ErrorMessage = $LTMError[0].message;
+        Throw ("We failed to remove the specified session. The error was: $ErrorMessage")
+
+    }
 }
